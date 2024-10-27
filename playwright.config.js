67d083511d8d591d3d6a29377ad8ba192efccd77
Nischal <nischal.pradhan20@gmail.com>
2024-10-27 07:30:57 +0000

    [SWAG-009] updated playwright yml

--- playwright.config.js
+++ playwright.config.js
@@ -7,11 +7,13 @@ const screenshotsDir = 'test-results/screenshots';
 if (!fs.existsSync(screenshotsDir)){
     fs.mkdirSync(screenshotsDir, { recursive: true });
 }
-
+if (!fs.existsSync(screenshotsDir)) {
+  fs.mkdirSync(screenshotsDir, { recursive: true });
+}
 module.exports = defineConfig({
   use: {
     baseURL: 'https://www.saucedemo.com/v1/index.html', // Define the base URL here
-    headless: false, // Optional: run browser in non-headless mode
+    headless: process.env.CI ? true : false, // Headless in CI environments
     viewport: { width: 1280, height: 720 },
     screenshot: 'only-on-failure', // Capture screenshots on failure
     video: 'retain-on-failure',   // Captures videos only on test failures
